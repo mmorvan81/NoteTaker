2571d7b9ceca0700bd3db7e476da59c2ab0487b6
Mary Morvan <mpmorvan@talbotdrake.com>
2021-05-14 14:49:46 -0400

    Add files via upload

--- server3050.js
+++ server3050.js
@@ -5,6 +5,7 @@ const fs = require("fs")
 const app = express();
 const dbJson = require("./db/db.json");
 const path = require("path");
+const { json } = require("express");
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -14,16 +15,13 @@ app.get("*", (request, response)=>{
     response.sendFile(path.join(__dirname, "./public/index.html")
     )}
 );
-app.get("/notes", (request, response)=>{
-    response.sendFile(path.join(__dirname, "./public/notes.html")
-    )}
-);
+
 app.get("/", (request, response)=>{
     response.sendFile(path.join(__dirname, "./public/index.html")
     )}
 );
 app.get("/api/notes", (request, response)=>{
-    response.JSON(dbJson.slice(1))
+    response.json(dbJson.slice(1))
     }
 );
 function createCurrentNote(main, noteBatch) {
@@ -35,12 +33,17 @@ function createCurrentNote(main, noteBatch) {
     noteBatch[0]++;
 
     noteBatch.push(currentNote);
-    fs.writeFileSync(path.join(__dirname, "./db/db.json"),JSON.stringify(noteBatch, null, 2));
+    fs.writeFileSync(path.join(__dirname, "./db/db.json"),json.stringify(noteBatch, null, 2));
     return currentNote;
 }
 app.post("/api/notes",(request, response)=> {
     const currentNote=createCurrentNote(request.main, dbJson);
-    response.JSON(currentNote);
+    response.json(currentNote);
 });
+
+app.get("/notes", (request, response)=>{
+    response.sendFile(path.join(__dirname, "./public/notes.html")
+    )}
+);
 app.listen(PORT, ()=> {
     console.log("The server is now listening on PORT" + PORT)});
